93d7979afc641c39ea6b8fa062d56b1a794c7f3d
MAG <mag_91@hotmail.de>
2022-07-08 10:51:32 +0200

    manager

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,26 @@
 const router = require("express").Router();
 const bcrypt = require('bcryptjs')
-
 const User = require("../models/User");
-
 const Vehicle = require("../models/Vehicle");
-
 // const Damage = require("../models/dammage")
-
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index", {layout: false});
+  res.render("index");
 });
- 
+//get manager page
+// router.get("/manager", (req,res) => {
+//   res.render("manager")
+// })
 router.get("/employee/delete/:userId", (req, res, next) => {
   User.findByIdAndRemove({_id: req.params.userId})
     .then(() => res.redirect("/employee"))
     .catch(error => console.log(error))
-  
 })
-
-/* GET manager page */
-router.get("/manager", (req, res, next) => {
-  res.render("manager");
-});
-
-
+router.get("/vehicle/delete/:vehicleId", (req, res, next) => {
+  Vehicle.findByIdAndRemove({_id: req.params.vehicleId})
+    .then(() => res.redirect("/vehicle"))
+    .catch(error => console.log(error))
+})
 /* GET employee/edite page */
 router.get("/employee/edit/:userId", (req, res) => {
   const userId = req.params.userId
@@ -44,104 +40,93 @@ router.get("/employee/edit/:userId", (req, res) => {
         res.render("edit", {userData, vehicleData, userId})
       })
    })
-   .catch(error => console.log(error)) 
+   .catch(error => console.log(error))
 })
-
-
 /* GET employee page */
 //access to the user from db
 router.get("/employee", (req, res, next) => {
-  
   User.find()
- 
    .populate("vehicle")
    .then(userData => {
-    // console.log("USERDATA =>", userData)
-    // Vehicle.find()
-      // .then((vehicleData) => {
-        res.render("employee", {userData})
-      // })
+      res.render("employee", {userData})
    })
-   .catch(error => console.log(error))  
+   .catch(error => console.log(error))
 });
-
-
-
 router.post('/employee', (req, res, next) => {
-	const {username, password, name, lastName, street, email, phone} = req.body
-	// console.log("req.body =>", req.body)
-	// validation
-	if (password.length < 4) {
-		res.render('employee', { message: 'Password has to be 4 chars min' })
-		return
-	}
-	// check if username is not empty
-	if (username === '') {
-		res.render('signup', { message: 'Username cannot be empty' })
-		return
-	}	
-	// validation passed
-	// check if that username already exists 
-	User.findOne({ username: username })
-		.then(userFromDB => {
-			// if there is a user
-			if (userFromDB !== null) {
-				res.render('signup', { message: 'Your username is already taken' })
-				return
-			} else {
-				// we can use that username
-				// we hash the password 
-				const salt = bcrypt.genSaltSync()
-				const hash = bcrypt.hashSync(password, salt)
-				// console.log(hash)
-				// create the user
-				User.create({username, password: hash, name, lastName, street, email, phone})
-					.then(createdUser => {
-						// console.log(createdUser)
-						res.redirect('/employee')
-					})
-					.catch(err => {
-						next(err)
-					})
-			}
-		})
+  const {username, password, name, lastName, street, email, phone} = req.body
+  // validation
+  if (password.length < 4) {
+    res.render('employee', { message: 'Password has to be 4 chars min' })
+    return
+  }
+  // check if username is not empty
+  if (username === '') {
+    res.render('signup', { message: 'Username cannot be empty' })
+    return
+  }
+  // validation passed
+  // check if that username already exists
+  User.findOne({ username: username })
+    .then(userFromDB => {
+      // if there is a user
+      if (userFromDB !== null) {
+        res.render('signup', { message: 'Your username is already taken' })
+        return
+      } else {
+        // we can use that username
+        // we hash the password
+        const salt = bcrypt.genSaltSync()
+        const hash = bcrypt.hashSync(password, salt)
+        // create the user
+        User.create({username, password: hash, name, lastName, street, email, phone})
+          .then(createdUser => {
+            // console.log(createdUser)
+            res.redirect('/employee')
+          })
+          .catch(err => {
+            next(err)
+          })
+      }
+    })
 });
-
-
-
-
 //get employee delete page
 // router.post("/employee/:userId/delete", (req, res) => {
 //   User.findByIdAndRemove({_id: req.params.userId})
 //     .then(() => res.sendStatus(204))
 //     .catch(error => console.log(error))
 // });
-  // "vehicle" => it's refer to the vehicle in user scema
- 
-
-
-
-
+// "vehicle" => it's refer to the vehicle in user scema
 //edite a user
 router.post("/employee/edit/:userId", (req, res) => {
-  const userId = req.params.userId
-  // console.log("req.body //edite=>", req.body);
+const userId = req.params.userId
 const {username, name, lastName, phone, email, vehicle} = req.body;
 let user = {
-  username, 
-  name, 
-  lastName, 
-  phone, 
+  username,
+  name,
+  lastName,
+  phone,
   email
 }
-
-// User.find()
-// .then(userFromDB => {
-//   if(vehicle)
-// })
-// .catch()
-
-
+let vehiclesNotAvailable = [];
+User.find()
+.then(users => {
+    users.forEach(user => {
+        if (user.vehicle) {
+            vehiclesNotAvailable.push(user.vehicle);
+        }
+    })
+    // console.log("vehiclesnotavailable: ", vehiclesNotAvailable)
+    Vehicle.find()
+    .then(vehicles => {
+        vehicles.forEach(vehicle => {
+            if (!vehiclesNotAvailable.includes(vehicle)) {
+                vehicle.available = true;
+                console.log("vehicle is available: ", vehicle);
+                vehicle.save();
+            }
+        })
+    })
+})
 if(vehicle) {
   user.available = false;
   user.vehicle = vehicle;
@@ -151,18 +136,10 @@ if(vehicle) {
    vehicleFromDB.save()
   })
 // console.log("VEHICLE ==>>", vehicle)
-
 }else {
   user.available = true;
   user.$unset = {vehicle: 1};
-  // user.vehicle = vehicle;
-  // Vehicle.findById(vehicle)
-  // .then(vehicleFromDB => {
-  //  vehicleFromDB.available = true;
-  //  vehicleFromDB.save()
 }
-// )}
-  // console.log("req.body.vehicle =>", req.body.vehic)
   User.findByIdAndUpdate(userId, user)
   .then(user => {
     res.redirect("/employee")
@@ -174,47 +151,45 @@ if(vehicle) {
   //       res.render("employee", {userData, vehicleData})
   //     })
   //  })
-   .catch(error => console.log(error))  
+   .catch(error => console.log(error))
 });
-
-
-
 /* GET vehicle page */
 //access to the vehicles from db
 router.get("/vehicle", (req, res, next) => {
     Vehicle.find()
     .then((vehicleData) => {
-      // console.log()
       res.render("vehicle", {vehicleData})
     })
     .catch(error => console.log(error))
-
-  // res.render("vehicle");
 });
-
-
-
-
-
-
-/* GET profile page */
-// router.get("/driver", (req, res, next) => {
-//   res.render("driver");
-// });
-
-
 /* GET manager page */
-// router.get("/manager", (req, res, next) => {
-//   User.find()
-//     .then(userData => {
-//       res.render("manager", {userData})
-//         console.log(userData)
-//     })
-    // .catch(err => console.log(error))
-
-  // res.render("manager");
-// });
-
-
-
+router.get("/manager", (req, res, next) => {
+  let availableDriver = [];
+  let availableVehicle = [];
+  User.find()
+    .then(userFromDB => {
+           userFromDB.forEach(user => {
+            if(user.available) {
+              console.log("availableDriver==>", user)
+              availableDriver.push(user)
+            }
+           })
+    })
+    Vehicle.find()
+    .then(vehicleFromDB => {
+      vehicleFromDB.forEach(vehicle => {
+       if(vehicle.available) {
+        availableVehicle.push(vehicle)
+       }
+      })
+      res.render("manager", {availableDriver, availableVehicle})
+})
+    .catch(err => console.log(error))
+});
+// User.find()
+// .then(users => {
+//     users.forEach(user => {
+//         if (user.vehicle) {
+//             vehiclesNotAvailable.push(user.vehicle);
+//         }
 module.exports = router;
